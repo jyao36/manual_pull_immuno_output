060abbe3c4412f4634b38fd5588cba790c7b8899
jyao36 <jennieyao036@gmail.com>
2026-03-23 15:34:41 -0500

    update script structure, README

--- 01_run_manual_build_outputs_json.sh
+++ 01_run_manual_build_outputs_json.sh
@@ -1,7 +1,7 @@
 # Change directory to the script directory
 cd "$(dirname "$0")"
 
-python3 "manual_build_outputs_json.py" \
+python3 "scripts/manual_build_outputs_json.py" \
   --template "template_outputs_pvac7.0.0b1.json" \
   --scratch-dir "/scratch1/fs1/mgriffit/jyao/pipeline_test/Hu_159_attempt2/cromwell-executions/immuno/fa804bb0-48fd-484b-8df3-f70e212fe756" \
   --sample-id "Hu_159" \
