d2368d33033019c609632fac7962eff0384663c3
jyao36 <jennieyao036@gmail.com>
2026-03-23 15:57:01 -0500

    update variables that users can change

--- 01_run_manual_build_outputs_json.sh
+++ 01_run_manual_build_outputs_json.sh
@@ -1,9 +1,13 @@
 # Change directory to the script directory
 cd "$(dirname "$0")"
 
+# Change these to the sample ID you are processing, and the large scratch directory where the immuno pipeline outputs are stored
+sample_id="Hu_159"
+immuno_dir_large="scratch1/fs1/mgriffit/jyao/pipeline_test/Hu_159_attempt2/cromwell-executions/immuno/fa804bb0-48fd-484b-8df3-f70e212fe756"
+
 python3 "scripts/manual_build_outputs_json.py" \
   --template "template_outputs_pvac7.0.0b1.json" \
-  --scratch-dir "/scratch1/fs1/mgriffit/jyao/pipeline_test/Hu_159_attempt2/cromwell-executions/immuno/fa804bb0-48fd-484b-8df3-f70e212fe756" \
-  --sample-id "Hu_159" \
+  --scratch-dir "${immuno_dir_large}" \
+  --sample-id "${sample_id}" \
   --template-sample-id "Hu_159" \
   --out-dir "results/outputs_json"
--- 02_run_manual_pull_outputs.sh
+++ 02_run_manual_pull_outputs.sh
@@ -1,9 +1,12 @@
 # Change directory to the script directory
 cd "$(dirname "$0")"
 
+# Change this to the sample ID you are processing
 sample_id="Hu_159"
+# Change this to the directory where the cloud-workflows repository is cloned
+cloud_workflows_dir="/storage1/fs1/mgriffit/Active/griffithlab/pipeline_test/jennie/ml_itb_test/cloud-workflows"
 
 bash scripts/manual_pull_outputs.sh \
---cloud-workflows-dir "/storage1/fs1/mgriffit/Active/griffithlab/pipeline_test/jennie/ml_itb_test/cloud-workflows" \
+--cloud-workflows-dir "${cloud_workflows_dir}" \
 --json-file "results/outputs_json/${sample_id}_outputs.json" \
 --outputs-dir "results/pull_outputs/${sample_id}_out"
